85dfec2575646fa31fd91e3f74f9fd5992e07478
Raytriccs <Rayaanxdutt@gmail.com>
2025-09-18 14:57:38 +0200

    Lag en liste med ordbøker

---
+++ ordbok.py
@@ -0,0 +1,9 @@
+# Steg 1 
+telefonbok = []
+
+person1 = {"navn": "Ada Lovelace", "nummer": "12345678"}
+person2 = {"navn": "Linus Torvalds", "nummer": "98765432"}
+
+telefonbok.append(person1)
+telefonbok.append(person2)
+
