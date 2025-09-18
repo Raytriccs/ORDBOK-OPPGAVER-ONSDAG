22aeab627830b52320a6b3f3da7b3558fe19bffe
Raytriccs <Rayaanxdutt@gmail.com>
2025-09-18 15:01:11 +0200

    Test programmet

--- ordbok.py
+++ ordbok.py
@@ -1,22 +1,3 @@
-# Oppgave 5 
-def hovedmeny():
-    while True:
-        print("\nMeny:")
-        print("1. Vis alle")
-        print("2. Legg til ny")
-        print("3. Søk")
-        print("4. Avslutt")
-        valg = input("Velg et tall: ")
-
-        if valg == "1":
-            vis_alle()
-        elif valg == "2":
-            legg_til()
-        elif valg == "3":
-            sok()
-        elif valg == "4":
-            print("Avslutter programmet.")
-            break
-        else:
-            print("Ugyldig valg, prøv igjen.")
+# Oppgave 6 
+hovedmeny()
 
